27ded2f6229bff48cb972cc082ed467bb28e01fd
Krystalone7 <kristallix2002@MAIL.RU>
2021-10-27 11:30:41 +0400

    Writer

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,14 @@
 import functions.*;
+import functions.basic.*;
+
+import java.io.File;
+import java.io.IOException;
+
+
 public class Main{
     public static void main(String[] args) {
-        double[] b = new double[5];
-        b[0] = 1.5;
-        b[1] = 4;
-        b[2] = 6;
-        b[3] = 5.7;
-        b[4] = 9;
-        ArrayTabulatedFunction f2 = new ArrayTabulatedFunction(1, 10, b);
-        /*for (int i = 0; i < f2.getPointsCount(); i++) {
-            System.out.println(f2.getPointX(i) + "   " + f2.getPointY(i));
-        }*/
-        FunctionPoint po = new FunctionPoint(5.5, 7);
-        try{
-            TabulatedFunction tab = new LinkedListTabulatedFunction();
+        Function cos = new Cos();
+        System.out.println(cos.getFunctionValue(1));
 
-
-        }
-        catch(Exception k){
-            System.out.println("Error");
-        }
     }
 }
--- src/functions/TabulatedFunctions.java
+++ src/functions/TabulatedFunctions.java
@@ -1,7 +1,7 @@
 package functions;
 
-import java.io.DataOutputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class TabulatedFunctions {
     public static TabulatedFunction tabulate(Function function, double leftX, double rightX, int pointsCount) throws IllegalArgumentException{
@@ -16,7 +16,29 @@ public class TabulatedFunctions {
         return new ArrayTabulatedFunction(points);
     }
     public static void outputTabulatedFunction (TabulatedFunction function, OutputStream out){
-        DataOutputStream dataOutputStream = new DataOutputStream(out);
-        //dataOutputStream.write();
+        try(DataOutputStream dataOutputStream = new DataOutputStream(out)) {
+            dataOutputStream.write(function.getPointsCount());
+            for (int i = 0; i < function.getPointsCount(); i++) {
+                dataOutputStream.write((" " + function.getPointX(i) + " " + function.getPointY(i)).getBytes(StandardCharsets.UTF_8));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+    public static TabulatedFunction inputTabulatedFunction(InputStream inputStream) {
+        FunctionPoint[] list = new FunctionPoint[0];
+        try (DataInputStream dataInputStream = new DataInputStream(inputStream)) {
+            int size = dataInputStream.readInt();
+            list = new FunctionPoint[size];
+            for (int j = 0; j < size; j++) {
+                int x = dataInputStream.readInt();
+                int y = dataInputStream.readInt();
+                list[j] = new FunctionPoint(x,y);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ArrayTabulatedFunction(list);
+    }
+
 }
